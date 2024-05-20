652c8b01e229bf4a55eb229c60fcf1b51adcf2b7
mArambasic <105987930+mArambasic@users.noreply.github.com>
2024-05-20 16:16:45 +0200

    story fix 2

--- stry_fe/src/Pages/Stories.tsx
+++ stry_fe/src/Pages/Stories.tsx
@@ -49,7 +49,7 @@ function useFetchStories() {
 
     const fetchData = async () => {
       try {
-        const res = await axios.post('https://infsus-fe.onrender.com/api/v1/getAllStories');
+        const res = await axios.get('https://infsus-fe.onrender.com/api/v1/getAllStories');
         if (res.status === 200) {
           const list = res.data;
           const stories: Story[] = list.map((story: any) => ({
