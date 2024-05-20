d354320b670c333f973502edf3c769e803511aa8
mArambasic <105987930+mArambasic@users.noreply.github.com>
2024-05-20 13:24:45 +0200

    fix

--- stry_be/src/main/java/stry/service/ReviewService.java
+++ stry_be/src/main/java/stry/service/ReviewService.java
@@ -3,15 +3,16 @@ package stry.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import stry.repository.BookCatalogRepository;
+import stry.repository.ReviewRepository;
 
 @Service
 public class ReviewService implements IReviewService {
     @Autowired
-    private ReviewService reviewService;
+    private ReviewRepository reviewRepository;
 
     public ReviewService() {}
 
-    public ReviewService(ReviewService reviewService) {
-        this.reviewService = reviewService;
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
     }
 }
